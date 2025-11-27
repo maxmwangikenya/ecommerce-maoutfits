a6fb5f3408045eb047c3bdb3d996f6871301379f
maxmwangikenya <maxmwangikenya@gmail.com>
2025-11-28 01:17:34 +0300

    listen to server

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const PORT = 5500;
+const dotenv = require("dotenv").config();
+
+
+mongoose.
+connect(process.env.MONGO_URI)
+.then(()=> {console.log("DBsuccessfully connected")})
+.catch((err) => {
+    console.log("err")
+})
+
+app.get("/api/test", () => {
+    console.log("test is successful")
+})
 
 app.listen(PORT, () =>{
     console.log(`server successfully running on port ${PORT}`)
